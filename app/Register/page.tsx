abf6f12298329cded1f8645910fb7c675bc19a46
EmpatiaDigital <empatiadigital2025@gmail.com>
2025-11-16 20:12:24 -0300

    Update page.tsx

---
+++ app/Register/page.tsx
@@ -0,0 +1,9 @@
+// Register.jsx
+export function Register() {
+return (
+<div className="p-6 bg-white rounded-2xl shadow-md max-w-md mx-auto text-center">
+<h2 className="text-3xl font-bold mb-4">Register</h2>
+<p className="text-gray-600">Componente de registro.</p>
+</div>
+);
+}
